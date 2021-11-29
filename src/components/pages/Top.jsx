cb8b2e6d5519baf0b3f01c9442288eeaf3b88af8
aotatsu8 <70871669+aotatsu8@users.noreply.github.com>
2021-11-29 16:20:57 +0900

    Initial commit

---
+++ src/components/pages/Top.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Top = () => {
+  return (
+    <SConponent>
+      <h2>TOPページです</h2>
+    </SConponent>
+  );
+};
+
+const SConponent = styled.div`
+  text-align: center;
+`;
